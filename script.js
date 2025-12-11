7f706e724b6399caf4f1696fabbd4c365992c3a2
alfrix <140119318+oalfrix@users.noreply.github.com>
2025-12-11 08:14:06 +0300

    upd

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 AOS.init({offset:1});
 
+
+
+const hamburg = document.querySelector('.hamburg')
+        const cancel = document.querySelector('.cancel')
+        const navbar = document.querySelector('.dropmenu');
+        
+        hamburg.addEventListener('click', ()=>{
+            navbar.style.transform = "translateY(0px)"
+            hamburg.style.display = 'none'
+        })
+        cancel.addEventListener('click', ()=>{
+            navbar.style.transform = "translateY(-300px)"
+            hamburg.style.display = 'block'
+        })
+
